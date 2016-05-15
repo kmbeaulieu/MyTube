c9e555ea119d229c13923d8a93018ef5bd043d86
Krystle Weinrich <krystleweinrich@yahoo.com>
2016-05-15 00:38:20 -0700

    committing to save got rid of fab button I think

--- app/src/main/java/sjsu/se137/group3/mytube/LoginActivity.java
+++ app/src/main/java/sjsu/se137/group3/mytube/LoginActivity.java
@@ -56,14 +56,14 @@ public class LoginActivity extends AppCompatActivity {
 
 
         //this isnt needed.
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
 }

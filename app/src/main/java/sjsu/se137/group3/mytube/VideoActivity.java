29c7d61bb3cc55081b40e5f1e4e314ea115b1184
SheethalSM <sheethalsmathew@gmail.com>
2016-05-15 22:42:45 -0700

    regular hardcoded video play view

---
+++ app/src/main/java/sjsu/se137/group3/mytube/VideoActivity.java
@@ -0,0 +1,17 @@
+package sjsu.se137.group3.mytube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
